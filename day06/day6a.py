91aaf43a2cf158cc684fc75a312c2990a160acfe
Kyle O'Malley <36799542+jkomalley@users.noreply.github.com>
2022-12-06 17:39:50 +0000

    Finish day 6

---
+++ day06/day6a.py
@@ -0,0 +1,13 @@
+"""Advent of Code day 6a"""
+def main():
+    """main function"""
+    with open("day06/day6_input.txt", encoding='utf8') as file:
+        datastream = file.readline()
+
+    for i in range(0, len(datastream)-1):
+        if len(set(datastream[i:i+4])) == 4:
+            print(i+4)
+            break
+
+if __name__ == "__main__":
+    main()
