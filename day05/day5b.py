aef66d7a8e0b258ad421a656ffb705d859894e33
Kyle O'Malley <36799542+jkomalley@users.noreply.github.com>
2022-12-06 07:56:48 +0000

    Finish day 5b

--- day05/day5b.py
+++ day05/day5b.py
@@ -1,10 +1,48 @@
-"""Advent of code day 5b"""
+"""Advent of code day 5a"""
+import re
+
+#     [B]             [B] [S]
+#     [M]             [P] [L] [B] [J]
+#     [D]     [R]     [V] [D] [Q] [D]
+#     [T] [R] [Z]     [H] [H] [G] [C]
+#     [P] [W] [J] [B] [J] [F] [J] [S]
+# [N] [S] [Z] [V] [M] [N] [Z] [F] [M]
+# [W] [Z] [H] [D] [H] [G] [Q] [S] [W]
+# [B] [L] [Q] [W] [S] [L] [J] [W] [Z]
+#  1   2   3   4   5   6   7   8   9
+stacks = [[],
+['B', 'W', 'N'],
+['L', 'Z', 'S', 'P', 'T', 'D', 'M', 'B'],
+['Q', 'H', 'Z', 'W', 'R'],
+['W', 'D', 'V', 'J', 'Z', 'R'],
+['S', 'H', 'M', 'B'],
+['L', 'G', 'N', 'J', 'H', 'V', 'P', 'B'],
+['J', 'Q', 'Z', 'F', 'H', 'D', 'L', 'S'],
+['W', 'S', 'F', 'J', 'G', 'Q', 'B'],
+['Z', 'W', 'M', 'S', 'C', 'D', 'J']]
+
+NUMBERS = "\d+"
+
 def main():
     """Main function"""
     with open("day05/day5_input.txt", encoding='utf8') as file:
         lines = file.readlines()
 
-    print(lines)
+    for line in lines:
+        count, src, dst = [int(i) for i in re.findall(NUMBERS, line)]
+
+        for i in stacks[src][-count:]:
+            stacks[dst].append(i)
+        
+        for i in stacks[src][-count:]:
+            stacks[src].pop()
+
+    # print(stacks)
+    for stack in stacks:
+        if len(stack) > 0:
+            print(stack.pop(), end="")
+
+    print()
 
 if __name__ == "__main__":
     main()
