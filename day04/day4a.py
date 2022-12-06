d8528d599f8b75975dcb1d9c4757b598d9aa0eb6
Kyle O'Malley <36799542+jkomalley@users.noreply.github.com>
2022-12-06 05:58:34 +0000

    Finish day 4a

--- day04/day4a.py
+++ day04/day4a.py
@@ -4,9 +4,21 @@ def main():
     with open("day04/day4_input.txt", encoding='utf8') as file:
         lines = file.readlines()
 
+    num_fully_contained = 0
+
     for line in lines:
-        range1, range2 = line.split(",")
-        print(range1, " ", range2)
+        range1, range2 = line.strip().split(",")
+
+        range1 = [int(i) for i in range1.split("-")]
+        range2 = [int(i) for i in range2.split("-")]
+
+        # print(range1, range2)
+        if range1[0] <= range2[0] and range1[1] >= range2[1] or \
+         range1[0] >= range2[0] and range1[1] <= range2[1]:
+            num_fully_contained += 1
+            # print("contained")
+    
+    print("Num fully contained: ", num_fully_contained)
 
 if __name__ == "__main__":
     main()
