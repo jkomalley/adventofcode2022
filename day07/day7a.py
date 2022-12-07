c3c00fe962bf6686ef88155b3b653d739b787988
Kyle O'Malley <36799542+jkomalley@users.noreply.github.com>
2022-12-07 04:59:21 +0000

    Start day 7

---
+++ day07/day7a.py
@@ -0,0 +1,10 @@
+"""Advent of Code Day 4a"""
+def main():
+    """main function"""
+    with open("day04/day4_input.txt", encoding='utf8') as file:
+        lines = file.readlines()
+
+    print(lines)
+
+if __name__ == "__main__":
+    main()
