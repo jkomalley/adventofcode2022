fea5b2e41f0d8d6ae01f53964e5e10f5705fbf23
Kyle O'Malley <36799542+jkomalley@users.noreply.github.com>
2022-12-09 20:07:42 +0000

    Finish day 9a

--- day09/day9a.py
+++ day09/day9a.py
@@ -13,10 +13,12 @@ grid = [[0,0,0,0,0,0],
 
 def main():
     """main function"""
-    with open("day09/day9_small_input.txt", encoding='utf-8') as file:
+    with open("day09/day9_input.txt", encoding='utf-8') as file:
         moves = [line.strip().split(" ") for line in file.readlines()]
 
     moves = [[move[0], int(move[1])] for move in moves]
+
+    tail_locations = []
 
     for move in moves:
         while move[1] != 0:
@@ -32,25 +34,39 @@ def main():
 
             update_tail()
 
-            print_grid()
+            tail_locations.append(" ".join(str(val) for val in tail))
+
+            # print_grid()
+    
+    num_visited = len(set(tail_locations))
+
+    print("Num tail visited: ", num_visited)
 
     return
 
 def update_tail():
     """update location of tail"""
     dis = distance(head, tail)
-    print("distance: ", dis)
+    # print("distance: ", dis)
     if dis > 1:
-        tail[0] = tail[0] + (head[0] - tail[0])
-        tail[1] = tail[1] + (head[1] - tail[1])
+        # x move
+        if tail[0] < head[0]:
+            tail[0] = tail[0] + 1
+        elif tail[0] > head[0]:
+            tail[0] = tail[0] - 1
+        # y move
+        if tail[1] < head[1]:
+            tail[1] = tail[1] + 1
+        elif tail[1] > head[1]:
+            tail[1] = tail[1] - 1
 
 def distance(point_a, point_b):
-    """ret distance between point_a and point_b"""
+    """ret floor of distance between point_a and point_b"""
     x_part = point_b[0] - point_a[0]
     x_part_squared = x_part * x_part
     y_part = point_b[1] - point_a[1]
     y_part_squared = y_part * y_part
-    return math.sqrt(x_part_squared + y_part_squared)
+    return math.floor(math.sqrt(x_part_squared + y_part_squared)) 
 
 def print_grid():
     """print grid"""
@@ -59,14 +75,10 @@ def print_grid():
     grid[tail[0]][tail[1]] = 2
     grid[head[0]][head[1]] = 1
     print("-"*100)
-    grid.reverse()
     for row in grid:
-        # row.reverse()
         for point in row:
             print(" ", point, end='')
         print()
-        # row.reverse()
-    grid.reverse()
     print("-"*100)
     grid[tail[0]][tail[1]] = 0
     grid[head[0]][head[1]] = 0
